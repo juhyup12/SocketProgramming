1b90c2738245f431a695323513c95b0564dd8e9c
juhyup12 <juhyup12@gmail.com>
2019-05-02 20:06:42 +0900

    Window 기반 Socket Programming 기본 :server ,client

---
+++ WindowSocket Programming/WinServer/main.cpp
@@ -0,0 +1,66 @@
+﻿
+
+#include <iostream>
+#include <WinSock2.h>
+using namespace std;
+
+void ErrorHandling(const char* message);
+
+int main(int argc, char* argv[])
+{
+	cout << argv[0] << endl;
+	cout << argc << endl;
+
+	WSADATA wsaData;
+	SOCKET hServSock, hCIntSock; // Socket을 2개만드네?
+	SOCKADDR_IN servAddr, cIntAddr; // Server 주소와 Client 주소정보 2개를 만드네?
+
+
+	int szCIntAddr;
+	char message[] = "Hello World"; // 메시지네?
+	if (argc != 2)
+	{
+		cout << "Usage : %s <port>" << argv[0] << endl;
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("WSAStartup() error!");
+	hServSock = socket(PF_INET, SOCK_STREAM, 0);
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;// IPV4
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY에 대해서
+	servAddr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+		ErrorHandling("bind() error");
+
+	if (listen(hServSock, 5) == SOCKET_ERROR)
+		ErrorHandling("listen() Error");
+
+	szCIntAddr = sizeof(cIntAddr);
+	hCIntSock = accept(hServSock, (SOCKADDR*)&cIntAddr, &szCIntAddr);// listen과 Accept의 차이..?  
+	//accept하면 Client의 소켓을 받아오나보다
+
+	if (hCIntSock == INVALID_SOCKET)
+	{
+		ErrorHandling("Accept() error!");
+	}
+
+	send(hCIntSock, message, sizeof(message), 0);
+	closesocket(hCIntSock);
+	closesocket(hServSock);
+	WSACleanup();
+	return 0;
+   
+}
+
+void ErrorHandling(const char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+
+}
+
---
+++ WindowSocket Programming/WindowSocket Programming/main.cpp
@@ -0,0 +1,71 @@
+﻿
+//Win Client
+
+#include <iostream>
+#include <stdlib.h>
+#include <WinSock2.h>
+void ErrorHandling(const char* message);
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+	SOCKET hSocket;
+	SOCKADDR_IN servAddr;// 소켓 주소
+
+	char message[30];
+	int strLen = 0;
+	int idx = 0, readLen = 0;
+
+	if (argc != 3)
+	{
+		cout << "Usage:" << argv[0] << "<IP> <port>" << endl;
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) // MAKEWORD(2,2)가 뭐였더라?
+	{
+		ErrorHandling("WSAStartup() error!");
+	}
+
+	hSocket = socket(PF_INET, SOCK_STREAM, 0); //Socket 생성 ,SOCK_STREAM 이뭐엿지?
+
+
+	if (hSocket == INVALID_SOCKET)
+		ErrorHandling("hSocket() error");
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = inet_addr(argv[1]);
+	servAddr.sin_port = htons(atoi(argv[2])); // atoi가뭐였더라 , argv는 어떻게나뉘는거엿지?
+
+	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) // connect 함수를통한 서버와 연결
+	{
+		ErrorHandling("connect() error!");
+	}
+
+	while (readLen = recv(hSocket, &message[idx++], 1, 0))// recv함수에서  길이를 나중에늘려보자
+	{
+
+		if (readLen == -1)
+		{
+			ErrorHandling("read() error!");
+		}
+		strLen += readLen;
+	}
+
+	cout << "Message from server :" << message << endl;
+	cout << "Function Read Call Count" << strLen << endl;
+
+	closesocket(hSocket);
+	WSACleanup();
+	return 0;
+
+}
+void ErrorHandling(const char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
